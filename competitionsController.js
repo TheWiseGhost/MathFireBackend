5cb342e62672c13cab45f03ae05d5ac687bd7a64
Aditya Byju <byjuaditya@gmail.com>
2024-04-20 12:59:38 -0500

    starting in_competition backend code

--- competitionsController.js
+++ competitionsController.js
@@ -25,11 +25,11 @@ export default class competitionCtrl {
       let competition = await competitionsDAO.getCompetition(id);
       console.log("CompetitionCtrl competition = " + JSON.stringify(competition))
 
-      if (!problem){
+      if (!competition){
         res.status(404).json({error: "Not found"});
         return;
       }
-      res.json(problem);
+      res.json(competition);
     } catch (e) {
       console.log(`GetCompetition: ${e}`);
       res.status(500).json({error: e});
